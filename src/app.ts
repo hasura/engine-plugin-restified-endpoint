09dbd68198c42c175d362fa63a6616dc04de4bf4
Toan Nguyen <hgiasac@gmail.com>
2025-10-16 00:22:29 +0700

    migrate opentelemetry api

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,11 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { Resource, resourceFromAttributes } from "@opentelemetry/resources";
+import {
+  ATTR_SERVICE_NAME,
+  SemanticResourceAttributes,
+} from "@opentelemetry/semantic-conventions";
 import { SpanStatusCode, context, propagation } from "@opentelemetry/api";
 import { W3CTraceContextPropagator } from "@opentelemetry/core";
 import { B3Propagator, B3InjectEncoding } from "@opentelemetry/propagator-b3";
@@ -35,20 +38,24 @@ if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
     }),
   );
 
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: "restified-endpoints-express",
-    }),
-  });
-
   const traceExporter = new OTLPTraceExporter({
     url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-    headers: {
-      Authorization: `pat ${process.env.OTEL_EXPORTER_PAT || ""}`,
-    },
+    headers: process.env.OTEL_EXPORTER_PAT
+      ? {
+          Authorization: `pat ${process.env.OTEL_EXPORTER_PAT}`,
+        }
+      : {},
   });
 
-  provider.addSpanProcessor(new SimpleSpanProcessor(traceExporter));
+  const resource = resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "restified-endpoints-express",
+  });
+
+  const provider = new NodeTracerProvider({
+    resource,
+    spanProcessors: [new SimpleSpanProcessor(traceExporter)],
+  });
+
   provider.register();
 
   registerInstrumentations({ instrumentations: [new HttpInstrumentation()] });
